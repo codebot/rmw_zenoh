e6b7ecb6b275ff05e85ce5fd4b3c5eaa5202fa45
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-10-24 17:48:25 +0200

    Fix unset request header writer GUID (#36)
    * Fix unset request header writer GUID
    
    * Fix unset request header writer GUID in `rmw_take_response`

--- rmw_zenoh_cpp/src/rmw_zenoh.cpp
+++ rmw_zenoh_cpp/src/rmw_zenoh.cpp
@@ -1874,6 +1874,8 @@ rmw_take_response(
     return RMW_RET_ERROR;
   }
 
+  memcpy(request_header->request_id.writer_guid, attachment.source_gid, RMW_GID_STORAGE_SIZE);
+
   request_header->source_timestamp = attachment.source_timestamp;
   if (request_header->source_timestamp < 0) {
     RMW_SET_ERROR_MSG("Failed to get source_timestamp from client call attachment");
@@ -2336,6 +2338,8 @@ rmw_take_request(
     RMW_SET_ERROR_MSG("Failed to get sequence_number from client call attachment");
     return RMW_RET_ERROR;
   }
+
+  memcpy(request_header->request_id.writer_guid, attachment.source_gid, RMW_GID_STORAGE_SIZE);
 
   request_header->source_timestamp = attachment.source_timestamp;
   if (request_header->source_timestamp < 0) {
