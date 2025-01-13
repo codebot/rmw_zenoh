1265a9a4c76490f43efe3431d95471d60148deaa
yuanyuyuan <az6980522@gmail.com>
2025-01-13 16:28:03 +0800

    refactor: remove the unneeded querying callbacks

--- rmw_zenoh_cpp/src/detail/graph_cache.hpp
+++ rmw_zenoh_cpp/src/detail/graph_cache.hpp
@@ -104,8 +104,6 @@ public:
   /// @brief Signature for a function that will be invoked by the GraphCache when a QoS
   ///   event is detected.
   using GraphCacheEventCallback = std::function<void (int32_t change)>;
-  /// Callback to be triggered when a publication cache is detected in the ROS Graph.
-  using QueryingSubscriberCallback = std::function<void (const std::string & queryable_prefix)>;
 
   /// @brief Constructor
   /// @param id The id of the zenoh session that is building the graph cache.
@@ -278,9 +276,6 @@ private:
   using GraphEventCallbackMap = std::unordered_map<std::size_t, GraphEventCallbacks>;
   // EventCallbackMap for each type of event we support in rmw_zenoh_cpp.
   GraphEventCallbackMap event_callbacks_;
-  // Map key expressions to a map of sub keyexpr_hash and QueryingSubscriberCallback.
-  std::unordered_map<std::string, std::unordered_map<std::size_t,
-    QueryingSubscriberCallback>> querying_subs_cbs_;
   std::mutex events_mutex_;
 
   // Mutex to lock before modifying the members above.
