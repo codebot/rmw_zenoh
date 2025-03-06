bd232167fc4ba9c71c544180e53116a16c256c0c
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-03-06 10:21:36 +0100

    review feedback
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rmw_zenoh_cpp/src/rmw_event.cpp
+++ rmw_zenoh_cpp/src/rmw_event.cpp
@@ -177,7 +177,7 @@ rmw_event_set_callback(
 
 ///=============================================================================
 bool
-rmw_event_check_compatible(rmw_event_type_t rmw_event_type)
+rmw_event_type_is_supported(rmw_event_type_t rmw_event_type)
 {
   return rmw_zenoh_cpp::event_check_compatible(rmw_event_type);
 }
